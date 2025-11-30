58a17852723bf17849469124a0615cfcebb98867
Oscar Castellanos <oscareliascastellanoscordero@gmail.com>
2025-11-30 14:40:32 -0600

    Colores personalizados

--- app/index.tsx
+++ app/index.tsx
@@ -4,15 +4,21 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 const App = () => {
   return (
     <SafeAreaProvider>
-      <View className='mt-6 mx-2.5'>
-        <Text 
-          style={{ fontFamily: 'WorkSans-Black' }}
-          className='text-5xl '>Hola mundo</Text>
+      <View className="mt-6 mx-2.5">
+        <Text style={{ fontFamily: "WorkSans-Black" }} className="text-5xl ">
+          Hola mundo
+        </Text>
 
-        <Text className='text-4xl font-work-black'>Hola mundo</Text>
-        <Text className='text-3xl font-work-medium'>Hola mundo</Text>
-        <Text className='text-2xl font-work-light'>Hola mundo</Text>
-        <Text className='text-xl'>Hola mundo</Text>
+        <Text className="text-4xl font-work-black text-primary">
+          Hola mundo
+        </Text>
+        <Text className="text-3xl font-work-medium text-secondary">
+          Hola mundo
+        </Text>
+        <Text className="text-2xl font-work-light text-secondary-100">
+          Hola mundo
+        </Text>
+        <Text className="text-xl text-tertiary">Hola mundo</Text>
       </View>
     </SafeAreaProvider>
   );
