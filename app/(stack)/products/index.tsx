1f6ba76b4cab2403472b8f02e95cadafb9e257d1
Oscar Castellanos <oscareliascastellanoscordero@gmail.com>
2025-11-30 17:05:45 -0600

    Listado de productos y detalle

--- app/(stack)/products/index.tsx
+++ app/(stack)/products/index.tsx
@@ -1,11 +1,29 @@
-import { Text, View } from 'react-native';
+import { products } from '@/store/products.store';
+import { Link } from 'expo-router';
+import { FlatList, Text, View } from 'react-native';
 
 const ProductScreen = () => {
   return (
-    <View>
-      <Text>products screen!!!</Text>
+    <View className="flex flex-1 px-4">
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View className="mt-10">
+            <Text className='text-2xl font-work-black'>{item.title}</Text>
+            <Text className=''>{item.description}</Text>
+
+            <View className='flex flex-row justify-between'>
+              <Text className='font-work-black'>{item.price}</Text>
+              <Link href={`/(stack)/products/${item.id}`} className='text-primary'>
+                Ver detalles
+              </Link>
+            </View>
+          </View>
+        )}
+      />
     </View>
-  )
+  );
 }
 
 export default ProductScreen;
